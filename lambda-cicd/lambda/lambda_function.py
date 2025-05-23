354d1baf0e0af22e2da90be39d2813169a636a3c
Mario Buber <mario.buber@buma-its.com>
2025-05-23 15:11:07 +0200

    update lambda_function.py

--- lambda-cicd/lambda/lambda_function.py
+++ lambda-cicd/lambda/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from from our CICD GHA workflow vs code!')
+        'body': json.dumps('Hello from from our CICD GHA workflow vs code! 🎯 BUMA')
     }
